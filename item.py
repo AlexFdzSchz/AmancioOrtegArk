0468c4225ff880a3c6e8a5b27a157d4fd6eb46ad
Alejandro Fernández <alexsolo007@gmail.com>
2023-06-04 21:14:20 +0200

    implementado clase item, json de items

--- item.py
+++ item.py
@@ -1,18 +1,23 @@
-from typing import List
-from item.py import Item
+from typing import List, TYPE_CHECKING
 
+if TYPE_CHECKING:
+    from item import Item
 class Item:
-    def __init__(self,reagents: List[Item], reagent_amounts: List[float], result_amount: int, energy: str, img: str):
+    def __init__(self, name: str, reagents: List, reagent_amounts: List[float], result_amount: int, crafting_tax: int, energy: str, exp: int):
+        self.name = name
         self.reagents = reagents
         self.reagent_amounts = reagent_amounts
         self.result_amount = result_amount
+        self.crafting_tax = crafting_tax
         self.energy = energy
-        self.img = img
+        self.exp = exp
         
-    def print_all():
+    def print_all(self):
+        print(self.name)
         print(self.reagents)
         print(self.reagent_amounts)
         print(self.result_amount)
+        print(self.crafting_tax)
         print(self.energy)
-        print(self.img)
+        print(self.exp)
         
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
-from item.py import Item
+from item import Item
+
 with open("items.json", "r", encoding="utf-8") as items_json:
     content = items_json.read()
 
@@ -11,10 +12,13 @@ for data in loadedData:
     reagents = data["reagents"]
     reagent_amounts = data["reagent_amounts"]
     result_amount = data["result_amount"]
+    crafting_tax = data["crafting_tax"]
     exp = data["exp"]
     energy = data["energy"]
-    img = data["img"]
 
     
-    item = Item(name, ignore, answer)
-    items.append(item)
+item = Item(name, reagents, reagent_amounts, result_amount, crafting_tax, energy, exp)
+items.append(item)
+
+for item in items:
+    item.print_all()
